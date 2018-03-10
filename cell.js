5f2671eea0bcdc5fd6eb2f84787afece0149bbb4
shaibam <shaibam@gmail.com>
2018-03-10 22:48:52 +0200

    grid cells selection fixed

--- cell.js
+++ cell.js
@@ -1,14 +1,24 @@
 class Cell {
-    constructor() {
+    constructor(id) {
         this._element = document.createElement('div');
         this._element.classList.add('cell','small');
+        this._element.innerHTML = id;
     }
     get element() {
         return this._element;
     }
-    grow() {
+    grow(row,column) {
         this._element.classList.remove('small')
         this._element.classList.add('big');
+        var o = {
+            'grid-row-start': row + 1,
+            'grid-row-end': row + 3,
+            'grid-column-start': column + 1,
+            'grid-column-end': column + 3
+        }
+        for (var i in o) {
+            this._element.style.setProperty(i, o[i])
+        }
     }
     shrink() {
         this._element.classList.remove('big')
--- grid.js
+++ grid.js
@@ -2,14 +2,14 @@ class Grid {
     constructor(container) {
         this._element = document.createElement('div');
         this._element.classList.add('grid');
-        this._cells=[];
+        this._cells = [];
         this._cellWidth = 300;
         this._cellHeight = 169;
         this._container = container;
         this._create();
         this._scaleRandomCellsInRow();
     }
-    
+
     get _count() {
         return Math.floor(this._element.offsetWidth / this._cellWidth) * Math.floor(this._element.offsetHeight / this._cellHeight);
     }
@@ -17,45 +17,51 @@ class Grid {
     get _containerCellCount() {
         return Math.floor(this._container.offsetWidth / this._cellWidth) * Math.floor(this._container.offsetHeight / this._cellHeight);
     }
-    
+
     get _columnsCount() {
         return Math.floor(this._element.offsetWidth / this._cellWidth);
     }
-    
+
     get _rowsCount() {
-        return Math.floor(this._element.offsetHeight/ this._cellHeight);
+        return Math.floor(this._element.offsetHeight / this._cellHeight);
     }
 
     _create() {
-        var tempContainer = document.createElement('div');
-        for (var i = 0; i < this._containerCellCount; i++) {
-            /*var dude =  createDude(i==3?'big':'small');
-            if (i==3)
-                window.dude=dude;*/
-            //var dude =  createDude(i!=r?'small':'big',i);
-            var c = new Cell();
+        this._container.appendChild(this._element);
+        var count = this._count;
+        console.log(count);
+        for (var i = 0; i < count - 1; i++) {
+            var c = new Cell(i);
             this._element.appendChild(c.element)
             this._cells.push(c);
-
-            /*dudes.push(dude);
-            rowElement.appendChild(dude);*/
         }
-        //this._element.innerHTML = tempContainer.innerHTML;
-        this._container.appendChild(this._element)        
+
     }
     _scaleRandomCellsInRow() {
-        for (var i = 0; i < this._rowsCount; i += 2) {
-            var r = Math.floor(Math.random() * this._columnsCount);
-            this._cells[r+(i*this._columnsCount)].grow();
-            //enlargeDude(dudes[r + (i * columns)], i, r);
-            //bigDudes.push(dudes[r + (i * columns)]);
+        var columnsCount = this._columnsCount;
+        var rowsCount = this._rowsCount;
+        /*var pushCount = 0;
+        for (var i = 0; i < rowsCount; i += 2) {
+            //for (var i = rowsCount-2; i >= 0; i -= 2) {
+            var r = Math.floor(Math.random() * (columnsCount - 1));
+            this._cells[r + (i * columnsCount)-pushCount].grow(i, r); // i is the row, r is the column
+            pushCount++;
+        }*/
+        var itemsCount=0;
+        var r;
+        for (var i =0;i<rowsCount;i+=2) {
+            r = Math.floor(Math.random() * (columnsCount - 1));
+            //this._cells[r + (i * columnsCount)].grow(i, r); // i is the row, r is the column
+            this._cells[itemsCount+r].grow(i, r); // i is the row, r is the column
+            itemsCount+=1+(columnsCount-2)*2;
+            rowsCount++;
         }
     }
-    
+
     get element() {
         return this._element;
     }
-    
+
     onResize() {
         this._scaleRandomCellsInRow()
     }
